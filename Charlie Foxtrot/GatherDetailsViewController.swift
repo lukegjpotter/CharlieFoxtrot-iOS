52d53ba37e3ccb0cad3878923f8998c1f89932b8
Luke GJ Potter <lukegjpotter@gmail.com>
2015-02-18 22:31:32 +0000

    Single View Application Working

--- Charlie Foxtrot/GatherDetailsViewController.swift
+++ Charlie Foxtrot/GatherDetailsViewController.swift
@@ -16,7 +16,98 @@ class GatherDetailsViewController: UIViewController {
     @IBAction func Convert(sender: UIButton) {
         self.TextToConvert.resignFirstResponder()
         
-        self.ConversionOutcome.text = TextToConvert.text
+        self.ConversionOutcome.text = Convert(TextToConvert.text)
+    }
+    
+    func Convert(ConvertText: String) -> String {
+        
+        var PhoneticRepresentation = ""
+        ConversionOutcome.numberOfLines = countElements(ConvertText)
+        
+        for character in ConvertText.lowercaseString {
+            
+            switch character {
+                
+            case "a":
+                PhoneticRepresentation += "Alpha"
+            case "b":
+                PhoneticRepresentation += "Bravo"
+            case "c":
+                PhoneticRepresentation += "Charlie"
+            case "d":
+                PhoneticRepresentation += "Delta"
+            case "e":
+                PhoneticRepresentation += "Echo"
+            case "f":
+                PhoneticRepresentation += "Foxtrot"
+            case "g":
+                PhoneticRepresentation += "Golf"
+            case "h":
+                PhoneticRepresentation += "Hotel"
+            case "i":
+                PhoneticRepresentation += "India"
+            case "j":
+                PhoneticRepresentation += "Juliett"
+            case "k":
+                PhoneticRepresentation += "Kilo"
+            case "l":
+                PhoneticRepresentation += "Lima"
+            case "m":
+                PhoneticRepresentation += "Mike"
+            case "n":
+                PhoneticRepresentation += "November"
+            case "o":
+                PhoneticRepresentation += "Oscar"
+            case "p":
+                PhoneticRepresentation += "Papa"
+            case "q":
+                PhoneticRepresentation += "Quebec"
+            case "r":
+                PhoneticRepresentation += "Romeo"
+            case "s":
+                PhoneticRepresentation += "Sierra"
+            case "t":
+                PhoneticRepresentation += "Tango"
+            case "u":
+                PhoneticRepresentation += "Uniform"
+            case "v":
+                PhoneticRepresentation += "Victor"
+            case "w":
+                PhoneticRepresentation += "Whiskey"
+            case "x":
+                PhoneticRepresentation += "Xray"
+            case "y":
+                PhoneticRepresentation += "Yankee"
+            case "z":
+                PhoneticRepresentation += "Zulu"
+            case "0":
+                PhoneticRepresentation += "Zero"
+            case "1":
+                PhoneticRepresentation += "One"
+            case "2":
+                PhoneticRepresentation += "Two"
+            case "3":
+                PhoneticRepresentation += "Three"
+            case "4":
+                PhoneticRepresentation += "Four"
+            case "5":
+                PhoneticRepresentation += "Five"
+            case "6":
+                PhoneticRepresentation += "Six"
+            case "7":
+                PhoneticRepresentation += "Seven"
+            case "8":
+                PhoneticRepresentation += "Eight"
+            case "9":
+                PhoneticRepresentation += "Niner"
+            default:
+                PhoneticRepresentation += ""
+            }
+            
+            PhoneticRepresentation += "\n"
+        }
+        
+        return PhoneticRepresentation
     }
 
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
