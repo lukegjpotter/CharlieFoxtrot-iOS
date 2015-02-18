9811c402f85327864f0f9e06d1303277cd88738c
Luke GJ Potter <lukegjpotter@gmail.com>
2015-02-18 19:21:07 +0000

    Initial Commit

---
+++ Charlie Foxtrot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Charlie Foxtrot
+//
+//  Created by Luke Potter on 18/02/2015.
+//  Copyright (c) 2015 Luke GJ Potter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
